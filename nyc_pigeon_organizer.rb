947aa560ddea4e878e03a8fa22de3f08e2beb3cb
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-20 17:53:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newhash = Hash.new
+  data.each do |key, description|
+    description.each do |secondkey, names|
+      names.each do |name|
+      if !newhash[name]
+        newhash[name] = {}
+      end
+      
+      if !newhash[name][key]
+        newhash[name][key] = []
+      end
+      
+      newhash[name][key] << secondkey.to_s 
+    end
+  end
+end
+return newhash
 end
